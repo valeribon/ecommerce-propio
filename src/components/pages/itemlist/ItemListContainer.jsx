a84c712c2af46a03ca20e1e1af83d782461e0de1
“Valeria” <valeribon@gmail.com>
2023-12-01 09:50:59 -0300

    Clear

--- src/components/pages/itemlist/ItemListContainer.jsx
+++ src/components/pages/itemlist/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { db } from "../../../firebaseConfig";
 import { getDocs, collection } from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 export const ItemListContainer = () => {
   const [products, setProducts] = useState([]);
@@ -12,8 +13,7 @@ export const ItemListContainer = () => {
         let newArray = res.docs.map((product) => {
           return { ...product.data(), id: product.id };
         });
-        console.log(res.docs);
-        console.log(newArray);
+        setProducts(newArray);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -21,6 +21,19 @@ export const ItemListContainer = () => {
   return (
     <div>
       <h1>Estoy en el SHOP</h1>
+
+      {products.map((product) => {
+        return (
+          <Link key={product.id}>
+            <div style={{ border: "2px solid black" }}>
+              <img src={product.image} style={{ width: "200px" }} alt="" />
+              <h4>{product.title}</h4>
+              <h4>{product.unit_price}</h4>
+              <h4>{product.stock}</h4>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
